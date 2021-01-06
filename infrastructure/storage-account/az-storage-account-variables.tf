147b9ce26159b567ac9e866bbc14d270d37033bc
Cheetah1989 <chetan2pune@yahoo.com>
2021-01-06 23:40:08 +0530

    added TF code

---
+++ infrastructure/storage-account/az-storage-account-variables.tf
@@ -0,0 +1,16 @@
+# General
+variable "location" {
+    type = string
+}
+variable "location_short_code" {
+    type = string
+}
+variable "shortcode" {
+    type = string
+}
+variable "product" {
+    type = string
+}
+variable "envname" {
+    type = string
+}
